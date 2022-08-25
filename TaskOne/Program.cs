4785210085b4cd3ee08c08f7f700837a6ce2ea50
Anton <altay77@inbox.ru>
2022-08-25 22:31:01 +0700

    TaskOne Solution

--- TaskOne/Program.cs
+++ TaskOne/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Write("Введите любое трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = num % 100;
+num = num / 10;
+Console.Write("Вторая цифра этого числа: "+num);
